956df2430acf316397a2a4d2cdc052fecfe5b86a
Marc Anell <marcanellespar@gmail.com>
2016-01-21 17:02:24 +0100

    Added consume definition

--- benchmark.go
+++ benchmark.go
@@ -4,6 +4,12 @@ import (
 	"time"
 )
 
+// FlowRunner is an interface that represents the ability to run a workflow
+type FlowRunner interface {
+	RunFlow(int, int) error
+	InitCollector(chan *Metric)
+}
+
 // Operation conatins info about an HTTP request
 type Operation struct {
 	Name   string
@@ -22,14 +28,64 @@ type Benchmark struct {
 	statsCollector chan *Metric
 	statsConsumers []chan *Metric
 	flow           FlowRunner
+	C              int
+	N              int
 }
 
+// NewBenchmark retuns a new instance of Benchmark
 func NewBenchmark(flow FlowRunner) *Benchmark {
+	statsConsumers := []chan *Metric{}
 
+	for _, consumer := range regConsumers {
+		c := make(chan *Metric, 4096)
+		statsConsumers = append(statsConsumers, c)
+		consumer.Run(c)
+	}
+
+	statsCollector := make(chan *Metric, 4096)
+	flow.InitCollector(statsCollector)
+
+	bench := &Benchmark{
+		statsCollector: statsCollector,
+		statsConsumers: statsConsumers,
+		flow:           flow,
+		C:              1,
+		N:              1,
+	}
+
+	return bench
 }
 
-// FlowRunner is an interface that represents the ability to run a workflow
-type FlowRunner interface {
-	RunFlow(int, int) error
-	InitCollector(chan *Metric)
+func (b *Benchmark) feedConsumers() {
+	for metric := range b.statsCollector {
+		for _, consumer := range b.statsConsumers {
+			consumer <- metric
+		}
+	}
+}
+
+// Run executes the benchmark
+func (b *Benchmark) Run() {
+	// Connect channels
+	go b.feedConsumers()
+
+	fi := make(chan int)
+
+	for j := 0; j < b.C; j++ {
+		go func(j, number int) {
+			for i := 0; i < number; i++ {
+				b.flow.RunFlow(i, j)
+			}
+			fi <- 1
+		}(j, b.N)
+	}
+
+	// wait
+	for j := 0; j < b.C; j++ {
+		<-fi
+	}
+
+	for _, consumer := range regConsumers {
+		consumer.Finalize()
+	}
 }
---
+++ consumer.go
@@ -0,0 +1,22 @@
+package benchmark
+
+// Consumer is an interface that represnets the ability to consume metrics and do
+// some stuff with them
+type Consumer interface {
+	Run(chan *Metric)
+	Finalize()
+}
+
+// regConsumers contains the available consumers
+var regConsumers = make(map[string]Consumer)
+
+// Register allows registering new consumers with an unique name
+func Register(name string, consumer Consumer) {
+	if consumer == nil {
+		panic("consumer: Register consumer is nil")
+	}
+	if _, dup := regConsumers[name]; dup {
+		panic("consumer: Register called twice for consumer " + name)
+	}
+	regConsumers[name] = consumer
+}
