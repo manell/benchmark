39176d34fd6012fb3d2f984186c09628ae5c148f
Marc Anell <marcanellespar@gmail.com>
2016-01-29 12:58:45 +0100

    Added a some tests

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,1 +1,68 @@
 package benchmark
+
+import (
+	"net/http"
+	"sync"
+	"testing"
+	"time"
+)
+
+type mockConsumers struct{}
+
+func (m *mockConsumers) Initialize(n, c int) {}
+func (m *mockConsumers) Pipe(input chan *Metric) chan int {
+	c := make(chan int, 1)
+	c <- 1
+	return c
+}
+func (m *mockConsumers) Finalize(d time.Duration) {}
+
+type mockCollector struct{}
+
+func (m *mockCollector) Initialize(chan *Metric)                 {}
+func (m *mockCollector) NewMeasure(string, *http.Request) func() { return func() {} }
+
+type mockFlow struct {
+	n     int
+	mutex *sync.Mutex
+}
+
+func (m *mockFlow) RunFlow(cli *BenchClient) error {
+	m.mutex.Lock()
+	m.n++
+	m.mutex.Unlock()
+
+	return nil
+}
+
+func TestBencharkNC(t *testing.T) {
+	tests := []struct {
+		n int
+		c int
+	}{
+		{0, 0},
+		{0, 1},
+		{1, 1},
+		{1, 10},
+		{10, 1},
+		{10, 10},
+		{100, 10},
+	}
+
+	for _, test := range tests {
+		b := &Benchmark{
+			N:                 test.n,
+			C:                 test.c,
+			DisableKeepAlives: false,
+			Consumers:         &mockConsumers{},
+			Collector:         &mockCollector{},
+		}
+
+		flow := &mockFlow{0, &sync.Mutex{}}
+		b.Run(flow)
+
+		if flow.n != test.n {
+			t.Fatal("Incorrect iterations value")
+		}
+	}
+}
